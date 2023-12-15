41d5e6d9dfe04cadf9e4648cd36e1b7616716756
Steffen <24982394+Stev-eH@users.noreply.github.com>
2023-12-15 14:33:45 +0100

    Added Polygon Manipulation

--- FaceTrackingWebcamExample.m
+++ FaceTrackingWebcamExample.m
@@ -47,6 +47,36 @@ frameSize = size(videoFrame);
 % Create the video player object. 
 videoPlayer = vision.VideoPlayer('Position', [100 100 [frameSize(2), frameSize(1)]+30]);
 
+% Creat a Slider
+% Create a figure
+slider = figure('Position', [100, 100, 400, 200], 'Name', 'RGB Sliders');
+
+% Initial RGB values
+initialRGB = [0, 0, 0]; % Adjust this as needed
+
+% Create sliders for R, G, and B components
+sliderR = uicontrol('Style', 'slider', 'Min', -255, 'Max', 255, 'Value', initialRGB(1), ...
+    'Units', 'normalized', 'Position', [0.1, 0.7, 0.8, 0.1], 'Callback', @sliderCallback);
+
+sliderG = uicontrol('Style', 'slider', 'Min', -255, 'Max', 255, 'Value', initialRGB(2), ...
+    'Units', 'normalized', 'Position', [0.1, 0.5, 0.8, 0.1], 'Callback', @sliderCallback);
+
+sliderB = uicontrol('Style', 'slider', 'Min', -255, 'Max', 255, 'Value', initialRGB(3), ...
+    'Units', 'normalized', 'Position', [0.1, 0.3, 0.8, 0.1], 'Callback', @sliderCallback);
+
+% Create a text box to display RGB values
+textLabel = uicontrol('Style', 'text', 'String', 'RGB Values: [0, 0, 0]', ...
+    'Units', 'normalized', 'Position', [0.1, 0.1, 0.8, 0.1]);
+
+% Set unique tags for each slider
+set(sliderR, 'Tag', 'sliderR');
+set(sliderG, 'Tag', 'sliderG');
+set(sliderB, 'Tag', 'sliderB');
+
+% Store textLabel in app data
+setappdata(slider, 'textLabel', textLabel);
+setappdata(slider, 'finalRGB', initialRGB);
+
 %% Detection and Tracking
 % Capture and process video frames from the webcam in a loop to detect and
 % track a face. The loop will run for 400 frames or until the video player
@@ -56,7 +86,7 @@ runLoop = true;
 numPts = 0;
 frameCount = 0;
 
-while runLoop && frameCount < 400
+while runLoop
     
     % Get the next frame.
     videoFrame = snapshot(cam);
@@ -118,13 +148,33 @@ while runLoop && frameCount < 400
             
             % Convert the box corners into the [x1 y1 x2 y2 x3 y3 x4 y4] 
             % format required by insertShape.
-            bboxPolygon = reshape(bboxPoints', 1, []);            
+            bboxPolygon = reshape(bboxPoints', 1, []);
             
-            % Display a bounding box around the face being tracked.
+            % Create a meshgrid of coordinates for the image
+            [xq, yq] = meshgrid(1:size(videoFrameGray, 2), 1:size(videoFrameGray, 1));
+            
+            % Check which points in the meshgrid are inside the polygon
+            insidePolygon = inpolygon(xq(:), yq(:), bboxPoints(:, 1), bboxPoints(:, 2));
+            
+            %access the modified RBG-Values
+            finalRGB = getappdata(slider, 'finalRGB');
+
+            % Flip the pixels manually for each color channel
+            for channel = 1:3  % assuming RGB image
+                channelPixels = videoFrame(:, :, channel);
+                if channel == 1
+                channelPixels(insidePolygon) = channelPixels(insidePolygon) + finalRGB(1);
+                end
+                if channel == 2
+                channelPixels(insidePolygon) = channelPixels(insidePolygon) + finalRGB(2);
+                end
+                if channel == 3
+                channelPixels(insidePolygon) = channelPixels(insidePolygon) + finalRGB(3);
+                end
+                videoFrame(:, :, channel) = channelPixels;
+            end
+
             videoFrame = insertShape(videoFrame, 'Polygon', bboxPolygon, 'LineWidth', 3);
-            
-            % Display tracked points.
-            videoFrame = insertMarker(videoFrame, visiblePoints, '+', 'MarkerColor', 'white');
             
             % Reset the points.
             oldPoints = visiblePoints;
@@ -145,6 +195,29 @@ clear cam;
 release(videoPlayer);
 release(pointTracker);
 release(faceDetector);
+
+
+% Callback function for the sliders
+function sliderCallback(src, ~)
+    % Access the figure handle from the source
+    slider = ancestor(src, 'figure');
+    % Access textLabel from app data
+    textLabel = getappdata(slider, 'textLabel');
+    
+    % Access the sliders' handles using findobj
+    sliderR = findobj(slider, 'Tag', 'sliderR');
+    sliderG = findobj(slider, 'Tag', 'sliderG');
+    sliderB = findobj(slider, 'Tag', 'sliderB');
+
+    redValue = get(sliderR, 'Value');
+    greenValue = get(sliderG, 'Value');
+    blueValue = get(sliderB, 'Value');
+    
+    % Update the displayed RGB values
+    set(textLabel, 'String', ['RGB Values: [', num2str(redValue), ', ', num2str(greenValue), ', ', num2str(blueValue), ']']);
+    setappdata(slider, 'finalRGB', [redValue, greenValue, blueValue]);
+    
+end
 
 %% References
 % Viola, Paul A. and Jones, Michael J. "Rapid Object Detection using a
